7c5318b1a1686605c9aa59f863a5f94d3654890e
Dayana Alejandro <dayana200120@gmail.com>
2023-05-09 16:53:08 -0400

    adding images to start

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 Requirements to run
-npm i react-router-dom
-npm install react-typist --save --legacy-peer-deps
-npm install --save react-audio-player
+* npm i react-router-dom
+* npm install react-typist --save --legacy-peer-deps
+* npm install --save react-audio-player
 
 Sources:
-Original Images
+* Original Images
 
 Sounds:
 * Home Page: https://freesound.org/people/szegvari/sounds/530705/
-*
--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
 import React, {useState} from 'react'
 import Typist from 'react-typist';
-import Sound from 'react-sound'
 import oceanWaves from './Static/ocean_waves.wav'
+import img1 from './images/start/IMG_8829.jpg'
+import img2 from './images/start/f990ff52.jpeg'
+import img3 from './images/start/IMG_1691.jpg'
+import img4 from './images/start/IMG_1835.jpg'
+
+import img5 from './images/start/IMG_2442.JPG'
+import img6 from './images/start/IMG_5945.JPG'
+import img7 from './images/start/IMG_6680.jpeg'
+import img8 from './images/start/IMG_6695.JPG'
+
+import img9 from './images/start/IMG_6697.JPG'
+import img10 from './images/start/IMG_6699.JPG'
+import img11 from './images/start/IMG_6700.JPG'
+import img12 from './images/start/IMG_6702.jpeg'
+
 
 function App() {
   const [startStatus, setstartStatus] = useState(false)
@@ -15,11 +29,37 @@ function App() {
     setstartStatus(true)
   }
 
-  if (startStatus==false){
+  if (!startStatus){
     return(
       <>
       <div id="start_home">
+        <div class="imgs_layout">
+          <img class="imageItems" src={img1} alt=""/>
+          <img class="imageItems" src={img2} alt=""/>
+          <img class="imageItems" src={img3} alt=""/>
+          <img class="imageItems" src={img4} alt=""/>
+        </div>
+
+        <div class="imgs_layout">
+          <img class="imageItems" src={img5} alt=""/>
+          <img class="imageItems" src={img6} alt=""/>
+        {/* </div> */}
+
+        <div id="start_background">
         <button id="start_button" onClick={changestart}>START</button>
+        </div>
+
+        {/* <div class="imgs_layout"> */}
+          <img class="imageItems" src={img7} alt=""/>
+          <img class="imageItems" src={img8} alt=""/>
+        </div>
+
+        <div class="imgs_layout">
+          <img class="imageItems" src={img9} alt=""/>
+          <img class="imageItems" src={img10} alt=""/>
+          <img class="imageItems" src={img11} alt=""/>
+          <img class="imageItems" src={img12} alt=""/>
+        </div>
       </div>
       </>
     )
