c4823d469f4a42f9a4a25f65b0caebfede41a272
Dayana Alejandro <dayana200120@gmail.com>
2023-05-08 22:32:58 -0400

    adding sound

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import './App.css';
+import React, {useState} from 'react'
 import Typist from 'react-typist';
+import Sound from 'react-sound'
+import oceanWaves from './Static/ocean_waves.wav'
 
 function App() {
-  return (
-    <div>
-      <div id="home_title">
-        <a id="home_button" href="/growing_up/#/p1">
-          <h1 id="title">
-            <Typist avgTypingDelay={100}> 
-              To a Better Future...
-            </Typist>
-          </h1>
-          </a>
-          
+  const [startStatus, setstartStatus] = useState(false)
+  const audio = new Audio(oceanWaves)
+  audio.loop = true
+  audio.volume = 0.3
+  audio.autoplay=true
+
+  let changestart=()=>{
+    setstartStatus(true)
+  }
+
+  if (startStatus==false){
+    return(
+      <>
+      <div id="start_home">
+        <button id="start_button" onClick={changestart}>START</button>
       </div>
-    </div>
-  );
+      </>
+    )
+  } else{
+    return (
+      <div>
+        <div id="home_title">
+          <a id="home_button" href="/growing_up/#/p1">
+            <h1 id="title">
+              <Typist avgTypingDelay={180}> 
+                To a Better Future...
+              </Typist>
+            </h1>
+          </a>
+        {/* <button onClick={()=>{
+          audio.pause()
+          audioStatus=false
+        }}>Pause</button> */}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
